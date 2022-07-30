66e40b43ac95dbac642556c7b4ebeb0bd758dc2d
shofol <18103156@iubat.edu>
2022-07-30 13:16:26 +0600

    binding

--- app.js
+++ app.js
@@ -6,7 +6,19 @@ createApp({
       message: "hello",
       info: {
         name: ["shofol", "shanto"],
+        gitLink: "https://github.com/shofol1/VJs",
       },
     };
   },
 }).mount("#app");
+
+const myJson = {
+  name: "shofol",
+  color: ["red", "green", "yellow"],
+  persons: [
+    { name: "shofol", address: [{ one: "saidpure", two: "dhaka" }] },
+    { name: "shanto", address: "Kustia" },
+    { name: "sohan", address: "Dhaka" },
+  ],
+};
+myJson.persons[0].address.map((a) => console.log(a.one));

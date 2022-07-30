e9b3a102f97abb1d7e24d1ef44f08b03038c2fc0
shofol <18103156@iubat.edu>
2022-07-30 21:51:43 +0600

    list item

---
+++ listBind/app.js
@@ -0,0 +1,32 @@
+const { createApp } = Vue;
+
+createApp({
+  data: function () {
+    return {
+      name: "hello",
+      newSkill: "",
+      skills: ["React", "javascript", "Html", "MERN Stack"],
+      persons: [
+        { name: "shofol", age: 24 },
+        { name: "shanto", age: 25 },
+        { name: "shofol", age: 23 },
+      ],
+      newPerson: {},
+    };
+  },
+  methods: {
+    addNew() {
+      if (this.newSkill) {
+        this.skills.push(this.newSkill);
+        this.newSkill = "";
+      }
+    },
+    removeSkill(i) {
+      this.skills.splice(i, 1);
+    },
+    addPerson() {
+      this.persons.push({ name: this.newPerson.name, age: 23 });
+      this.newPerson = "";
+    },
+  },
+}).mount("#app");
